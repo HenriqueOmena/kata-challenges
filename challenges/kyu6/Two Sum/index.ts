9f4848791a44dbb0c00b94229250e7e144f7f8f1
HenriqueOmena <88lmao@gmail.com>
2020-12-20 23:24:07 +0000

    done optmization into code

--- challenges/kyu6/Two Sum/index.ts
+++ challenges/kyu6/Two Sum/index.ts
@@ -15,4 +15,22 @@ const twoSum = (numbers: number[], target: number): number[] => {
 };
 
 console.log(twoSum([1, 3, 9, 7, 2], 11), "expected [3,4]");
-console.log(twoSum([2, 27, 3, 123], 9), "expected [1,2]");
+console.log(twoSum([2, 7, 3, 123], 9), "expected [1,2]");
+
+const twoSumOptimized = (numbers: number[], target: number): number[] => {
+  const numbersMap = {};
+  for (let p = 0; p < numbers.length; p++) {
+    const currentMapValue = numbersMap[numbers[p]];
+
+    if (currentMapValue >= 0) {
+      return [currentMapValue, p];
+    } else {
+      const numberToFind = target - numbers[p];
+      numbersMap[numberToFind] = p;
+    }
+  }
+  return null;
+};
+
+console.log(twoSumOptimized([1, 3, 9, 7, 2], 11), "expected [3,4]");
+console.log(twoSumOptimized([2, 7, 3, 123], 9), "expected [1,2]");
